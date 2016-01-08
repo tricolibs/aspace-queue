32ac2c38d057352e43b92b4c2befbad6bf19a70c
cfitz <chrisfitzpat@gmail.com>
2016-01-08 13:01:16 +0100

    just some comments

--- indexer/indexer.rb
+++ indexer/indexer.rb
@@ -1,9 +1,12 @@
 require 'resque'
 
 
+# this is the job call that is used by resque. 
 class CreateJob
+  # the name of the queue we want in Redis. This can be anything we want. 
   @queue = :record_created
 
+  # this is called when the job is enqueue. you can do other fun stuff here, if you want
   def self.perform
     begin
       $stderr.puts "PUTTING IT ON THE Q!"
@@ -14,16 +17,16 @@ class CreateJob
 
 end
 
-
+# this overrides our indexers
 class CommonIndexer
 
+  # we add a new hook when the indexeres are created
   add_indexer_initialize_hook do |indexer|
+    # and we add a hook that happens when the record is being prepared for indexing
+    # record = the ASPACE record
+    # doc = the document being put to SOLR
     indexer.add_document_prepare_hook {|doc, record|
-      $stderr.puts "*" * 100
-      $stderr.puts record.inspect 
-      $stderr.puts Resque.enqueue(CreateJob, record)
-      $stderr.puts "*" * 100
-
+      Resque.enqueue(CreateJob, record)
     }
   end
 
