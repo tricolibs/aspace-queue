6e6a628dca91b411fde70e9ae0160c19056888c2
Laurin Penland <lpenland@brynmawr.edu>
2023-06-08 11:43:56 -0400

    connecting to trico messaging server, fixing bugs, removing resque

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
 ASpaceGems.setup if defined? ASpaceGems
-gem 'resque'
+gem 'bunny', ">= 2.9.2"
--- indexer/indexer.rb
+++ indexer/indexer.rb
@@ -1,24 +1,63 @@
-require 'resque'
+require 'bunny'
+require 'json'
 
 
-# this is the job call that is used by resque. 
-class CreateJob
-  # the name of the queue we want in Redis. This can be anything we want. 
-  @queue = :record_created
+# # this is the job call that is used by resque. 
+# class CreateJob
+#   # the name of the queue we want in Redis. This can be anything we want. 
+#   @queue = :record_created
 
-  # this is called when the job is enqueue. you can do other fun stuff here, if you want
-  def self.perform
-    begin
-      $stderr.puts "PUTTING IT ON THE Q!"
-    rescue => e
-      $stderr.puts e.backtrace
-    end 
+#   # this is called when the job is enqueue. you can do other fun stuff here, if you want
+#   def self.perform
+#     begin
+#       $stderr.puts "PUTTING IT ON THE Q!"
+#     rescue => e
+#       $stderr.puts e.backtrace
+#     end 
+#   end
+
+# end
+
+# # this overrides our indexers
+# class CommonIndexer
+
+#   # we add a new hook when the indexeres are created
+#   add_indexer_initialize_hook do |indexer|
+#     # and we add a hook that happens when the record is being prepared for indexing
+#     # record = the ASPACE record
+#     # doc = the document being put to SOLR
+#     indexer.add_document_prepare_hook {|doc, record|
+#       Resque.enqueue(CreateJob, record)
+#     }
+#   end
+
+# end
+
+class Publish
+  attr_accessor :conf, :conn, :ch, :queue
+  
+  def initialize 
+    initialize_bunny
+  end
+
+  def initialize_bunny
+    self.conn = Bunny.new(host: 'message.tricolib.brynmawr.edu', user: AppConfig[:message_user], pass: AppConfig[:message_password], vhost: AppConfig[:message_vhost])
+    conn.start
+    self.ch = conn.create_channel
+    self.queue = ch.queue(AppConfig[:message_queue], durable: true)
+  end
+
+  def add_record(record)
+    message = record.to_json
+    queue.publish(message, persistent: true)
+    logger = Logger.new(STDOUT)
+    logger.info { "Is the indexer plugin working?"}
   end
 
 end
 
 # this overrides our indexers
-class CommonIndexer
+class IndexerCommon
 
   # we add a new hook when the indexeres are created
   add_indexer_initialize_hook do |indexer|
@@ -26,7 +65,7 @@ class CommonIndexer
     # record = the ASPACE record
     # doc = the document being put to SOLR
     indexer.add_document_prepare_hook {|doc, record|
-      Resque.enqueue(CreateJob, record)
+      Publish.new.add_record(record)
     }
   end
 
